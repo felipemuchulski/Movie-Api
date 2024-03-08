2e15a03a5039981e7946dbfd61129b53338a1af6
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-03-07 21:20:35 -0300

    Criada tabela de usuários

---
+++ src/database/postgresql/migrations/createUser.js
@@ -0,0 +1,13 @@
+const createUser = `{
+    CREATE TABLE users(
+        id_user SERIAL PRIMARY KEY,
+        name_user VARCHAR(266) NOT NULL,
+        email_user VARCHAR(266) NOT NULL,
+        password_user VARCHAR(266),
+        avatar VARCHAR(266),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        update_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+}`;
+
+module.exports = createUser;
