6da2e32d240b0ea00bbb21af2c5a614d95a6fa83
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-03-20 21:40:55 -0300

    Configurado arquivo Knex

---
+++ knexfile.js
@@ -0,0 +1,54 @@
+// Update with your config settings.
+const path = require("path")
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      user: "postgres",
+      password: "91584072",
+      host: "localhost",
+      port: 5432,
+      database: "Movie_API"
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
