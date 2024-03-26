233367bab3474936043898e415cd4e3be20c63d9
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-03-25 21:27:37 -0300

    Configurado knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,21 @@
 // Update with your config settings.
-const path = require("path")
+
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+const path = require("path")
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
       user: "postgres",
       password: "91584072",
       host: "localhost",
       port: 5432,
       database: "Movie_API"
+
     },
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
