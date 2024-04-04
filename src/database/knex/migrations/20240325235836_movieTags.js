24f7f4029b786940523a9bc46b49c1f597350c9f
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-04-04 14:18:53 -0300

    updated

--- src/database/knex/migrations/20240325235836_movieTags.js
+++ src/database/knex/migrations/20240325235836_movieTags.js
@@ -5,15 +5,15 @@ const { table } = require("..");
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = (knex) => knex.schema.createTable("movie_tags", (table) => {
+exports.up = (knex) =>
+  knex.schema.createTable("movie_tags", (table) => {
     table.increments("movie_tag_id");
-    
-    table.integer("user_id").references("id_users").inTable("users");
+
+    table.integer("user_id").references("id_user").inTable("users");
     table.integer("movies_id").references("id_movies").inTable("movie_notes");
     table.text("name_tag").notNullable();
-})
-  
-;
+
+  });
 
 /**
  * @param { import("knex").Knex } knex
