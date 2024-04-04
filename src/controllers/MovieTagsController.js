3357e3846460ef86cf6cafc6b0681f0e4b06c16d
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-04-04 14:20:26 -0300

    Criado arquivo para configura;'ao de tags

---
+++ src/controllers/MovieTagsController.js
@@ -0,0 +1,14 @@
+const knex = require("../database/knex")
+const connectionString = require("../database/postgresql/connect_database");
+
+class MoviesNotesController {
+    async index(request, response) {
+        const {user_id} = request.params
+
+        const tags = await connectionString.query("SELECT * FROM tags WHERE id_user = $1"[user_id])
+
+        return response.json(tags.rows);
+    }
+}
+
+module.exports = MoviesNotesController;
