ea677ebd660f7443c65ac34de542bbe8fb1e8505
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-04-04 22:53:01 -0300

    Efetuada alterção de request.params

--- src/routes/movieNotes.routes.js
+++ src/routes/movieNotes.routes.js
@@ -5,6 +5,7 @@ const moviesRoutes = Router();
 const movieNotesController = new MovieNotesController();
 
 moviesRoutes.post("/", movieNotesController.create);
+moviesRoutes.get("/:id_movies", movieNotesController.show);
 
 module.exports = moviesRoutes;
 
