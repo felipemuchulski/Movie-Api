8b8ca002e10b08395f54b56b7db28e83430292a0
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-04-04 22:52:43 -0300

    Efetuada configuração para exibir o filme e descrição

--- src/controllers/MovienotesController.js
+++ src/controllers/MovienotesController.js
@@ -28,6 +28,43 @@ class MovieNotesController {
       response.status(error.statusCode || 400).json({ error: error.message });
     }
   }
+
+  async show(request, response){
+    const { id_movies } = request.params
+
+    try {
+      const movieNote =  await connectionString.query("SELECT * FROM movie_notes WHERE id_movies = $1 LIMIT 1", [id_movies]);
+
+      
+      const movieDescription = await connectionString.query("SELECT description FROM movie_notes WHERE id_movies = $1", [id_movies]);
+     
+      
+      if (
+        movieNote.rows.length > 0 &&
+         movieDescription.rows.length > 0 
+      ) {
+        const movie = movieNote.rows[0];
+        const description = movieDescription.rows[0]
+        
+        return response.json({
+         ... movie,
+          description
+        });
+      } else {
+        return response.status(404).json({ AppError: `nota nao encontrada` });
+        
+      }
+
+
+    } catch (error) {
+      console.error("Erro ao buscar a nota:", error);
+      return response.status(500).json({ error: "Erro interno do servidor" });
+    }
+  }
+
+  async index(request, response){
+    const {title, user_id, tags } = request.params
+  }
 }
 
 module.exports = MovieNotesController;
